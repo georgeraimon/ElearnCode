eca819f8d83b0c6e3ee3a69dd8ea5b7f4d87a545
georgeraimon <122098083+georgeraimon@users.noreply.github.com>
2023-12-12 22:32:20 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,14 +11,15 @@ export default defineConfig({
   "compilerOptions": {
     "types": ["ace-builds"]
   }
-   chunkSizeWarningLimit:1500,
- rollupOptions: {
-        output:{
-            manualChunks(id) {
-              if (id.includes('node_modules')) {
-                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
-              }
-          }
+   build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
         }
     }
 })
