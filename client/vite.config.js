b7dbfbac4f006b637489ac8ae8d9d7f9a1f64cbb
georgeraimon <122098083+georgeraimon@users.noreply.github.com>
2023-12-12 22:48:22 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,18 +11,7 @@ export default defineConfig({
   "compilerOptions": {
     "types": ["ace-builds"]
   },
-   build: {
-    chunkSizeWarningLimit: 16000,
-        rollupOptions: {
-            output:{
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
-    }
+  
 })
 /*import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
