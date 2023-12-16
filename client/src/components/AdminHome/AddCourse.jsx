ce543e71037b76d55ee60c2ac86826ab26a9b0cf
georgeraimon <122098083+georgeraimon@users.noreply.github.com>
2023-12-16 22:04:55 +0530

    Update AddCourse.jsx

--- client/src/components/AdminHome/AddCourse.jsx
+++ client/src/components/AdminHome/AddCourse.jsx
@@ -63,7 +63,7 @@ export default function AddCourse() {
             setError(true);
             setErrorMessage(data.error);
           } else {
-            navigate("/admin_home");
+            navigate("/admin_home/view/:id"   ");
           }
         });
     }
